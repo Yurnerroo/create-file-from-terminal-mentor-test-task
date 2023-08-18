387549a51782e6995156c85d4847338ecf77058d
Yurnerroo <yura.zdanevych@gmail.com>
2023-08-18 14:39:27 +0300

    code refactor

--- app/create_file.py
+++ app/create_file.py
@@ -1,56 +1,65 @@
+import os
 from datetime import datetime
 from sys import argv
 from os import mkdir, chdir
 
-filename = ""
-directories = []
 
-if "-f" in argv:
-    index = 0
+def parse_args(command_line_args: list) -> tuple:
+    filename: str = ""
+    directories = list()
 
-    for _ in range(len(argv) - 1):
-        index += 1
-        if argv[_: _ + 1] == ["-f"]:
+    if "-f" in command_line_args:
+        index = command_line_args.index("-f")
+        filename = command_line_args[index + 1]
+
+    if "-d" in command_line_args:
+        index = command_line_args.index("-d")
+        if "-f" in command_line_args:
+            directories = command_line_args[index + 1: command_line_args.index("-f")]
+        else:
+            directories = command_line_args[index + 1: -1]
+            filename = command_line_args[-1]
+
+    return filename, directories
+
+
+def get_file_data() -> list:
+    file_data: list = [datetime.now().strftime("%Y-%d-%m %H:%M:%S"), "\n"]
+    row_index = 1
+
+    while True:
+        line = input("Enter content line: ")
+        if line == "exit":
             break
+        file_data.extend([f"{row_index} ", line, "\n"])
+        row_index += 1
 
-    filename = argv[index]
+    return file_data
 
-    if "-d" in argv:
-        index2 = 0
 
-        for _ in range(len(argv) - 1):
-            index2 += 1
-            if argv[_: _ + 1] == ["-d"]:
-                break
+def create_directories(directories: list) -> None:
+    for directory in directories:
+        try:
+            mkdir(path=directory)
+        except (FileExistsError, PermissionError, OSError):
+            pass
+        chdir(path=directory)
 
-        directories = argv[index2:index - 1]
-else:
-    filename = "file.txt"
 
-    if "-d" in argv:
-        index = 0
+def write_file(filename: str, file_data: list) -> None:
+    mode = "a" if os.path.exists(path=filename) else "w"
+    with open(file=filename, mode=mode) as f:
+        if mode == "a":
+            f.write("\n")
+        f.writelines(file_data)
 
-        for _ in range(len(argv) - 1):
-            index += 1
-            if argv[_: _ + 1] == ["-d"]:
-                break
 
-        directories = argv[index:len(argv)]
+def main(command_line_args: list) -> None:
+    filename, directories = parse_args(command_line_args=command_line_args)
+    file_data: list = get_file_data()
+    create_directories(directories=directories)
+    write_file(filename=filename, file_data=file_data)
 
-file_data = [datetime.today().strftime('%y:%d:%m %H-%M-%S'), "\n"]
 
-while True:
-    line = input("Enter content line: ")
-    if line == "exit":
-        break
-    file_data.extend([line])
-
-for directory in directories:
-    try:
-        mkdir(directory)
-    except Exception:
-        pass
-    chdir(directory)
-
-with open(filename, "w") as f:
-    f.writelines(file_data)
+if __name__ == "__main__":
+    main(command_line_args=argv)
